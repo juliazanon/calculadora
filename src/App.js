9662e9d17dd782dd63327b7bc1afb0d293bde5ea
Julia Harnisch <julia_zanon@usp.br>
2020-10-11 16:46:20 -0300

    Adicionar layout de memória

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends React.Component {
     this.state = {
       input: "",
       input2: "",
-      calculado: 'false',
-      decimal: 'false',
-      operador: 'false',
-      digitar_junto: 'true'
+      calculado: false,
+      decimal: false,
+      operador: false,
+      digitar_junto: true
     };
   }
 
@@ -23,48 +23,49 @@ class App extends React.Component {
     // Se é um ponto
     if (val === ".") {
       // se já tiver um ponto não escreve nada
-      if (this.state.decimal === 'true') {
-        this.setState({input: this.state.input});
-        this.setState({input2: this.state.input2});
-      }
+      if (this.state.decimal === true) {}
       else {
         // Se já teve uma operação, ou input vazio
-        if (this.state.calculado === 'true' || this.state.input === "") {
+        if (this.state.calculado === true || this.state.input === "") {
           val = "0."
-          this.setState({input: val});
-          this.setState({input2: val});
-          this.setState({calculado: 'false'});
-          // Marca que já tem ponto
-          this.setState({decimal: 'true'});
+          this.setState({
+            input: val,
+            input2: val,
+            calculado: false,
+            decimal: true});
         }
         else {
-            this.setState({input: this.state.input + val});
-            this.setState({input2: this.state.input2 + val});
-            // Marca que já tem ponto
-            this.setState({decimal: 'true'});
+            this.setState(state => ({
+              input: this.state.input + val,
+              input2: this.state.input2 + val,
+              decimal: true}));
         }
       }
     }
     // Se é um número
     if (!isNaN(val)) {
       // Se já teve uma operação
-      if (this.state.calculado === 'true' || this.state.input === "") {
-        this.setState({input: val});
-        this.setState({input2: val});
-        this.setState({calculado: 'false'});
+      if (this.state.calculado === true || this.state.input === "") {
+        this.setState({
+          input: val,
+          input2: val,
+          calculado: false,
+          digitar_junto: true});
       }
       else {
-        // Se  deve digitar os números juntos
-        if (this.state.digitar_junto === 'true') {
-          this.setState({input: this.state.input + val});
-          this.setState({input2: this.state.input2 + val});
-          this.setState({calculado: 'false'});
+        // Se deve digitar os números juntos
+        if (this.state.digitar_junto === true) {
+          this.setState(state => ({
+            input: this.state.input + val,
+            input2: this.state.input2 + val,
+            calculado: false}));
         }
         else {
-          this.setState({input: val});
-          this.setState({input2: this.state.input2 + val});
-          this.setState({digitar_junto: 'true'});
-          this.setState({calculado: 'false'});
+          this.setState(state => ({
+            input: val,
+            input2: this.state.input2 + val,
+            digitar_junto: true,
+            calculado: false}));
         }
       }
     }
@@ -72,48 +73,41 @@ class App extends React.Component {
     if (val === " X " || val === " + " || val === " - " || val === " ÷ ") {
       if (val === " X ") {
         val = " * ";
-        this.setState({input2: this.state.input2 + val});
-
-        // Indica que foi inserido um operador
-        this.setState({operador: 'true'});
-        // Remove a restrição do ponto
-        this.setState({decimal: 'false'});
-        // Marca que se deve digitar um número novo após um operador
-        this.setState({digitar_junto: 'false'});
-        this.setState({calculado: 'false'});
+        this.setState(state => ({
+          input2: this.state.input2 + val,
+          operador: true,
+          decimal: false,
+          digitar_junto: false,
+          calculado: false}));
       }
+
       if (val === " ÷ ") {
         val = " / "
-        this.setState({input2: this.state.input2 + val});
-
-        // Indica que foi inserido um operador
-        this.setState({operador: 'true'});
-        // Remove a restrição do ponto
-        this.setState({decimal: 'false'});
-        // Marca que se deve digitar um número novo após um operador
-        this.setState({digitar_junto: 'false'});
-        this.setState({calculado: 'false'});
+        this.setState(state => ({
+          input2: this.state.input2 + val,
+          operador: true,
+          decimal: false,
+          digitar_junto: false,
+          calculado: false}));
       }
-      else {
-        this.setState({input2: this.state.input2 + val});
 
-        // Indica que foi inserido um operador
-        this.setState({operador: 'true'});
-        // Remove a restrição do ponto
-        this.setState({decimal: 'false'});
-        // Marca que se deve digitar um número novo após um operador
-        this.setState({digitar_junto: 'false'});
-        this.setState({calculado: 'false'});
+      else {
+        this.setState(state => ({
+          input2: this.state.input2 + val,
+          operador: true,
+          decimal: false,
+          digitar_junto: false,
+          calculado: false}));
       }
     }
   }
 
   calcular = () => {
-    this.setState({input: math.evaluate(this.state.input2)});
-    this.setState({calculado: 'true'});
-    this.setState({decimal: 'false'});
-    // Marca que se deve digitar um número novo após uma operação
-    this.setState({digitar_junto: 'false'});
+    this.setState(state => ({
+      input: math.evaluate(this.state.input2),
+      calculado: true,
+      decimal: false,
+      digitar_junto: false}));
   }
 
   acoesMemoriaGeral = (val, ) => {
@@ -142,12 +136,12 @@ class App extends React.Component {
         <div className="box-calc">
           <Input input={this.state.input2}></Input>
           <Input input={this.state.input}></Input>
-          {/* <div id="operacoes" className="linha">
+          <div id="operacoes" className="linha">
             <Botao handleClick={this.acoesMemoriaGeral}> MC </Botao>
             <Botao handleClick={this.acoesMemoriaGeral}> MR </Botao>
             <Botao handleClick={this.acoesMemoriaGeral}> M+ </Botao>
             <Botao handleClick={this.acoesMemoriaGeral}> MS </Botao>
-          </div> */}
+          </div>
           <div id="operacoes" className="linha">
             <Botao handleClick={this.escreverInput}> + </Botao>
             <Botao handleClick={this.escreverInput}> - </Botao>
@@ -178,29 +172,45 @@ class App extends React.Component {
             </div>
           </div>
         </div>
-        {/* <div className="box-memoria">
+        <div className="box-memoria">
           <h1>Memória</h1>
           <div className="linha">
-            <Input memoria1={this.state.memoria1} id="input-memoria"></Input>
-            <Botao handleClick={this.acoesMemoria1}> MC </Botao>
-            <Botao handleClick={this.acoesMemoria1}> MR </Botao>
+            <div className="input-memoria">
+              <Input memoria1={this.state.memoria1}></Input>
+            </div>
+            <div className="acoes-memoria">
+              <Botao handleClick={this.acoesMemoria1}> MC </Botao>
+              <Botao handleClick={this.acoesMemoria1}> MR </Botao>
+            </div>
           </div>
           <div className="linha">
-            <Input memoria1={this.state.memoria2} id="input-memoria"></Input>
-            <Botao handleClick={this.acoesMemoria2}> MC </Botao>
-            <Botao handleClick={this.acoesMemoria2}> MR </Botao>
+            <div className="input-memoria">
+              <Input memoria2={this.state.memoria2}></Input>
+            </div>
+            <div className="acoes-memoria">
+              <Botao handleClick={this.acoesMemoria2}> MC </Botao>
+              <Botao handleClick={this.acoesMemoria2}> MR </Botao>
+            </div>
           </div>
           <div className="linha">
-            <Input memoria1={this.state.memoria2} id="input-memoria"></Input>
-            <Botao handleClick={this.acoesMemoria3}> MC </Botao>
-            <Botao handleClick={this.acoesMemoria3}> MR </Botao>
+            <div className="input-memoria">
+              <Input memoria3={this.state.memoria3}></Input>
+            </div>
+            <div className="acoes-memoria">
+              <Botao handleClick={this.acoesMemoria3}> MC </Botao>
+              <Botao handleClick={this.acoesMemoria3}> MR </Botao>
+            </div>
           </div>
           <div className="linha">
-            <Input memoria1={this.state.memoria4} id="input-memoria"></Input>
-            <Botao handleClick={this.acoesMemoria4}> MC </Botao>
-            <Botao handleClick={this.acoesMemoria4}> MR </Botao>
+            <div className="input-memoria">
+              <Input memoria4={this.state.memoria4}></Input>
+            </div>
+            <div className="acoes-memoria">
+              <Botao handleClick={this.acoesMemoria4}> MC </Botao>
+              <Botao handleClick={this.acoesMemoria4}> MR </Botao>
+            </div>
           </div>
-        </div> */}
+        </div>
       </div>
     );
   }
